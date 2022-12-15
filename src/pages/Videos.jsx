e07f8ed1c9f6eb8b9fd53c74ab49e1cbd71391a4
Gose <niabc70@gmail.com>
2022-12-15 19:07:44 +0900

    Youtube API 연결

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+// usequery 비동기로 받아오는 상태를 관리하는 라이브러리이다.
+// 즉 행동은 우리가 따로 작성해야 한다.
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "../components/VideoCard";
+// import FakeYoutube from "../api/fakeYoutube";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+// import Youtube from "../api/youtube";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
+  // 로딩중일때, 에러일때, data videos를 받아올때 usequery를 사용한다.
+  // 전체적인 key videos안에 keyword별로 key를 만든다.
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
 
-  return <div>Videos {keyword ? `👀${keyword}` : "👍"}</div>;
+  return (
+    <>
+      <div>Videos {keyword ? `👀${keyword}` : "👍"}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 👎👎</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
