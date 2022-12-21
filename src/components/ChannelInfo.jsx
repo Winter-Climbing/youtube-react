299a4a890c89df9f67038db34c77169cb8f3f5f4
Gose <niabc70@gmail.com>
2022-12-21 17:41:56 +0900

    네트워크 staletime 설정

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -4,9 +4,11 @@ import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function ChannelInfo({ id, name }) {
   const { youtube } = useYoutubeApi();
-  const { data: url } = useQuery(["channel", id], () => {
-    youtube.channelImageURL(id);
-  });
+  const { data: url } = useQuery(
+    ["channel", id],
+    () => youtube.channelImageURL(id),
+    { staleTime: 1000 * 60 * 5 }
+  );
   return (
     <div className="flex my-4 mb-8 items-center">
       {url && <img className="w-10 h-10 rounded-full" src={url} alt={name} />}
